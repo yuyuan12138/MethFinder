66d2f496dc84957c82130a5a0e0f06c2f42b83c9
meigum1n <103375793+meigum1n@users.noreply.github.com>
2025-12-24 09:40:05 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .save_load_model import load_model, save_model
-from .set_seed import set_seed
-from .save_pickles import delete_all_previous_folder_files, save_umap_pickles
-from.drawing import draw_acc_loss_line, draw_umap
